6e939818f69d19e18a1d6795207dcf3da38a00a8
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2025-10-27 18:51:34 +0200

    h1.3 valmis

---
+++ H1_3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    cout << "Anna kokonaisluku: " << endl;
+    cin>>num1;
+    //Jos kahdella jaettaessa jakojäännös on nolla
+    if(num1 % 2 == 0){
+        cout<<"Luku "<<num1<<" on parillinen"<<endl;
+    }
+    else {
+        cout<<"Luku "<<num1<<" on pariton"<<endl;
+    }
+    //cin>>num1;
+    return 0;
+}
