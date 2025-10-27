f0c7bc4984224409b30fb9503f1e0252879245c7
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2025-10-27 18:28:33 +0200

    luentotehtävä 1.1 valmis

---
+++ H1_1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+    cout << "Anna nimesi: ";
+    cin >> name;
+    cout << "Anna ikasi: ";
+    cin >> age;
+    cout << "Terve " << name << "olet siis " << age << " vuotias" << endl;
+    cin >>age;
+    return 0;
+}
