2f0d91b7b889410568db1e55769a6c1dabdba792
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2025-11-17 17:50:42 +0200

    deleteMe deletoitu

--- muistivuoto/main.cpp
+++ muistivuoto/main.cpp
@@ -4,6 +4,8 @@ int main() {
     for (int i = 0; i < 5; i++) {
         Dog* dog = new Dog("Koira" + to_string(i));
         dog->bark();
+        delete dog;
+        dog=nullptr;
         // VIRHE: delete puuttuu!
         // Jokaisella kierroksella vuotaa muistia
     }
